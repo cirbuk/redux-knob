b07c03549c2ba2b955fbf25e7ecdaa1dc5dd5c25
tstreamDOTh <tstream.h@outlook.com>
2019-08-13 23:04:15 +0530

    Add recipes & update test cases

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import { BATCH_TYPE } from "./constants";
 
+const defaultOptions = { batchType: BATCH_TYPE };
+
 export const composeReducers = (reducers = [], defaultState = {}) => (state = defaultState, action) =>
 	reducers.reduce((state, reducer) => reducer(state, action), state);
 
 export const compose = (functions = [], defaultState = {}) => functions.reduce((state, fn) => fn(state), defaultState);
 
-export const enableBatching = (reducer, { batchType = BATCH_TYPE }) => (state, action) => {
+export const enableBatching = (reducer, options = {}) => (state, action) => {
+	const { batchType } = { ...defaultOptions, ...options };
 	if (action.type === batchType) {
 		let { payload = [] } = action;
 		if (!Array.isArray(payload)) {
